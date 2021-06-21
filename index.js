e6ead89b646757ddd65bf99008c9a78625bb2c6e
budlebee <bbogle7613@gmail.com>
2021-06-21 16:55:37 +0900

    todo: 이미지를 base64 인코딩한다음 json 배열 형태로 return하기

--- index.js
+++ index.js
@@ -1,80 +1,154 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1280, height: 800 });
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
 
-  // 1시간 30분짜리 영상
-  //await page.goto(`https://www.youtube.com/watch?v=o3ka5fYysBM&t=${5}s`, {
-  //  waitUntil: "networkidle2",
-  //});
-
-  // 34분짜리 영상
-  await page.goto(`https://www.youtube.com/watch?v=OrxmtDw4pVI&t=${5}s`, {
-    waitUntil: "networkidle2",
-  });
-
-  // 1분 미만 영상
-  //await page.goto(`https://www.youtube.com/watch?v=lg2zZRLQU1A&t=${5}s`, {
-  //  waitUntil: "networkidle2",
-  //});
-
-  await page.waitForSelector(".html5-main-video");
-  const videoEle = await page.$(".html5-main-video");
-  if (await page.$(".paused-mode")) {
-    await page.keyboard.press("k");
-  }
-  const timeEle = await page.$(".ytp-bound-time-right");
-  const lenText = await timeEle.evaluate((ele) => {
-    return ele.innerHTML;
-  }, timeEle);
-  const lenArray = lenText.split(":");
-
-  let videoLength;
-  if (lenArray[0] == 0) {
-    console.log("too short!");
-  } else if (lenArray.length == 2) {
-    videoLength = lenArray[0];
-  } else if (lenArray.length == 3) {
-    console.log(lenArray);
-    videoLength = parseInt(lenArray[0], 10) * 60 + parseInt(lenArray[1], 10);
-    console.log(videoLength);
-  } else {
-    console.log("exeptional case");
-  }
-  const captionBtn = await page.$(".ytp-subtitles-button");
-  const captionOn = await page.evaluate(
-    (el) => el.getAttribute("aria-pressed"),
-    captionBtn
-  );
-  if (captionOn == "false") {
-    await captionBtn.click();
-  }
-
-  // full screep 버튼이 안눌린다..왜지.
-  const fullscreenBtn = await page.$(".ytp-fullscreen-button");
-
-  await fullscreenBtn.click();
-
-  await page.waitForTimeout(100);
-
-  for (let i = 0; i < videoLength; i++) {
-    await page.waitForSelector(".buffering-mode", { hidden: true });
-    await videoEle.screenshot({
-      path: `results/${i}_${Date.now()}.jpeg`,
-      type: "jpeg",
-      quality: 50,
+    // 1시간 30분짜리 영상
+    await page.goto(`https://www.youtube.com/watch?v=o3ka5fYysBM&t=5s`, {
+      waitUntil: "networkidle2",
     });
-    await page.keyboard.press("l");
-    await page.keyboard.press("l");
-    await page.keyboard.press("l");
-    await page.keyboard.press("l");
-    await page.keyboard.press("l");
-    await page.keyboard.press("l");
-    await page.waitForTimeout(100);
+
+    // 34분짜리 영상
+    //await page.goto(`https://www.youtube.com/watch?v=OrxmtDw4pVI&t=5s`, {
+    //  waitUntil: "networkidle2",
+    //});
+
+    // 1분 미만 영상
+    //await page.goto(`https://www.youtube.com/watch?v=lg2zZRLQU1A&t=${5}s`, {
+    //  waitUntil: "networkidle2",
+    //});
+
+    // 즉석 테스트
+    //await page.goto(`https://www.youtube.com/watch?v=I8M3GjGxRNA&t=5s`, {
+    //  waitUntil: "networkidle2",
+    //});
+
+    // get video component
+    await page.waitForSelector(".html5-main-video");
+    const videoEle = await page.$(".html5-main-video");
+
+    if ((await page.$(".paused-mode")) !== null) {
+      await page.keyboard.press("k");
+    }
+
+    // get video length
+    const timeEle = await page.$(".ytp-bound-time-right");
+    const lenText = await timeEle.evaluate((ele) => {
+      return ele.innerHTML;
+    }, timeEle);
+    const lenArray = lenText.split(":");
+    let videoLength;
+    if (lenArray[0] === "0") {
+      console.log("too short!");
+      console.log(lenArray);
+    } else if (lenArray.length == 2) {
+      console.log("under 1 hour video");
+      videoLength = lenArray[0];
+    } else if (lenArray.length == 3) {
+      console.log("over 1 hour video");
+      videoLength = parseInt(lenArray[0], 10) * 60 + parseInt(lenArray[1], 10);
+    } else {
+      console.log("exeptional case");
+    }
+
+    // caption on
+    const captionBtn = await page.$(".ytp-subtitles-button");
+    const captionOn = await page.evaluate(
+      (el) => el.getAttribute("aria-pressed"),
+      captionBtn
+    );
+    if (captionOn == "false") {
+      await captionBtn.click();
+    }
+
+    // full screep 버튼이 안눌린다..왜지.
+    //const fullscreenBtn = await page.$(".ytp-fullscreen-button");
+    //
+    //await fullscreenBtn.click();
+
+    // start screenshot
+    for (let i = 0; i < videoLength; i++) {
+      await page.waitForSelector(".buffering-mode", { hidden: true });
+
+      try {
+        await page.waitForFunction(
+          `document.querySelector('.ytp-spinner') && document.querySelector('.ytp-spinner').style.display=='none'`
+        );
+      } catch (e) {
+        console.log("스피너 대기하는곳에서 에러발생");
+      }
+
+      // ad skip
+      try {
+        while ((await page.$(".ad-showing")) !== null) {
+          await page.waitForSelector(".buffering-mode", { hidden: true });
+          if ((await page.$(".paused-mode")) !== null) {
+            await page.keyboard.press("k");
+          }
+          await page.waitForTimeout(6000);
+          //await page.waitForSelector(".ytp-ad-skip-button");
+          if ((await page.$(".ytp-ad-skip-button")) !== null) {
+            const skipButton = await page.$(".ytp-ad-skip-button");
+            await skipButton.click();
+          }
+        }
+      } catch (e) {
+        console.log("광고 스킵하는 부분에서 에러발생");
+      }
+
+      await page.waitForSelector(".buffering-mode", { hidden: true });
+
+      // close overlay ad
+      try {
+        if ((await page.$(".ytp-ad-overlay-close-button")) !== null) {
+          const adCloseBtns = await page.$$(".ytp-ad-overlay-close-button");
+          await adCloseBtns[adCloseBtns.length - 1].click();
+        }
+      } catch (e) {
+        console.log("오버레이 광고 끄는 부분에서 에러 발생");
+      }
+
+      try {
+        if ((await page.$(".paused-mode")) !== null) {
+          await page.keyboard.press("k");
+        }
+      } catch (e) {
+        console.log("플레잉 모드인거 재생멈춤 하는데서 에러발생");
+      }
+
+      // take screenshot
+      await videoEle.screenshot({
+        path: `results/${i}_${Date.now()}.jpeg`,
+        type: "jpeg",
+      });
+
+      // move to next time
+      await page.keyboard.press("l");
+      await page.keyboard.press("l");
+      await page.keyboard.press("l");
+      await page.keyboard.press("l");
+      await page.keyboard.press("l");
+      await page.keyboard.press("l");
+      await page.waitForTimeout(200);
+      await page.waitForSelector(".buffering-mode", { hidden: true });
+      try {
+        await page.waitForFunction(
+          `document.querySelector('.ytp-spinner') && document.querySelector('.ytp-spinner').style.display=='none'`
+        );
+      } catch (e) {
+        console.log("맨 마지막 스피너 기다리는데서 에러발생");
+      }
+    }
+
+    // all done. close browser.
+    await browser.close();
+  } catch (e) {
+    console.log(e);
   }
-  await browser.close();
+
   // lenText 를 : 기준으로 자르고, 원소 갯수가 2,3이냐에 따라 분기처리.
   // 만약 원소갯수가 2개다-> 1시간 아래의 영상임. 이때 첫번째 원소를 숫자로 바꾼게 1보다 작으면 너무 짧다고 반려.
   // 그외의 경우엔 처리.
